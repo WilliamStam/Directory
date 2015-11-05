13019beaf1a4e6ebfe9ef0d6d1acb058c5dc4dc4
William Stam <william@impreshin.com>
2015-11-05 21:21:43 +0200

    latest

--- update/db_update.php
+++ update/db_update.php
@@ -1,12 +1,17 @@
 <?php
 $sql = array(
-	//	"ALTER TABLE  `lin_directory_cat` CHANGE  `dirID`  `itemID` INT( 6 ) NULL DEFAULT NULL;",
-	//	"RENAME TABLE  `lin_directory` TO  `dir_items` ;",
-	//	"RENAME TABLE  `lin_directory_categories` TO  `dir_categories` ;",
-	//	"RENAME TABLE  `lin_directory_cat` TO  `dir_item_category` ;",
-     //   "ALTER TABLE `dir_categories` DROP `domainID`;",
-	//	"ALTER TABLE `dir_items` DROP `domainID`;",
-	//	""
+	/*
+	DELETE FROM `lin_directory` WHERE domainID != 16;
+	DELETE FROM `lin_directory_categories` WHERE domainID != '16';
+	
+	ALTER TABLE  `lin_directory_cat` CHANGE  `dirID`  `itemID` INT( 6 ) NULL DEFAULT NULL;
+	RENAME TABLE  `lin_directory` TO  `dir_items` ;
+	RENAME TABLE  `lin_directory_categories` TO  `dir_categories` ;
+	RENAME TABLE  `lin_directory_cat` TO  `dir_item_category` ;
+	ALTER TABLE `dir_categories` DROP `domainID`;
+	ALTER TABLE `dir_items` DROP `domainID`;
+	  
+	 */
 )
 
 ?>
